4cb401c28bd4b83952d24e7c95d155ee8dccf15b
Devin <derpvin12@gmail.com>
2023-05-10 15:31:38 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+import Checkout from "./views/Checkout";
+import HairProducts from "./views/HairProducts";
+import Login from "./views/Login";
+import OrderTracking from "./views/OrderTracking";
+
+const routes = [
+  {
+    path: "/",
+    element: <HairProducts />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/tracking",
+    element: <OrderTracking />,
+  },
+];
+
 function App() {
   return (
     <>
-      <div className="container"></div>
+      <div className="container">
+        <Routes>
+          {routes.map((route) => (
+            <Route {...route} />
+          ))}
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/views/HairProducts/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HairProducts() {
+  return <div>HairProducts</div>;
+}
