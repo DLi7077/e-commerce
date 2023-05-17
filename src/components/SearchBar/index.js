caff18b7ea09fd1783d5f483e1e20765f8d2aeae
butterriolu <derpvin12@gmail.com>
2023-05-17 18:49:04 -0400

    Add search bar

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,28 @@
+import { Autocomplete, TextField } from "@mui/material";
+import React from "react";
+
+export default function SearchBar(props) {
+  return (
+    <Autocomplete
+      freeSolo
+      options={props.options}
+      disableClearable
+      style={{
+        width: "min(400px,100%)",
+        marginBottom: "1rem",
+        marginLeft: "auto",
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search for an item"
+          InputProps={{
+            ...params.InputProps,
+            type: "search",
+            color: "primary",
+          }}
+        />
+      )}
+    />
+  );
+}
--- src/views/HairProducts/index.js
+++ src/views/HairProducts/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import { HAIR_WAX } from "../../constants/hair-products";
 import ProductCard from "../../components/ProductCard";
 import classes from "./hair-products.module.scss";
+import SearchBar from "../../components/SearchBar";
 
 export default function HairProducts() {
   return (
-    <div className={classes["grid"]}>
-      {HAIR_WAX.map((product) => (
-        <ProductCard key={product.name} {...product} />
-      ))}
+    <div>
+      <SearchBar options={HAIR_WAX.map((product) => product.name)} />
+      <div className={classes["grid"]}>
+        {HAIR_WAX.map((product) => (
+          <ProductCard key={product.name} {...product} />
+        ))}
+      </div>
     </div>
   );
 }
