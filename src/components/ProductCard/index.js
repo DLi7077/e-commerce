b9b38c0bbe4c256af13b69875593b2270e6ad920
butterriolu <derpvin12@gmail.com>
2023-05-17 15:18:41 -0400

    Add price and add-to-cart button

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import classes from "./product-card.module.scss";
 import ReviewStars from "./ReviewStars";
+import { IconButton } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 export default function ProductCard(product) {
   return (
     <div className={classes["card-container"]}>
-      <div className={classes["image-container"]}>
-        <img
-          src={product.image_url}
-          className={classes["product-image"]}
-          alt={product.name}
-        />
-        <div className={classes["product-brand"]}>{product.brand}</div>
+      <div>
+        <div className={classes["image-container"]}>
+          <img
+            src={product.image_url}
+            className={classes["product-image"]}
+            alt={product.name}
+          />
+          <div className={classes["product-brand"]}>{product.brand}</div>
+        </div>
+        <div className={classes["product-price"]}>${product.price}</div>
       </div>
       <div>
         <a
@@ -27,6 +32,17 @@ export default function ProductCard(product) {
           <span className={classes["review-count"]}>({product.rating})</span>
         </div>
       </div>
+      <IconButton
+        sx={{
+          position: "absolute",
+          bottom: "0.25rem",
+          right: "0.25rem",
+          color: "primary.highlight",
+          padding: "0.25rem",
+        }}
+      >
+        <AddCircleOutlineIcon />
+      </IconButton>
     </div>
   );
 }
